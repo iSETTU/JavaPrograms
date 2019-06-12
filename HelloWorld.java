4f574efda163775075e324c4badc3b1aecaa57ff
Settu <9102740@newgenkw.local>
2019-06-12 21:00:47 +0530

    My Second Commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package afirstJavaProgram;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("HelloWorld!");
+		System.out.println("This is My First Java Program!");
+		
+	}
+}
